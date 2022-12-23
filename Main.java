2a2b3e1d919d6b3805925a5d674a3af65a227b7f
John Doe <mangalkunchalwar9999@gmail.com>
2022-12-23 10:41:27 +0530

    welcome to the Address Book Management System Development

---
+++ Main.java
@@ -0,0 +1,7 @@
+package assignment10.AddressBookManagementSystemDevelopment;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("welcome to the Address Book Management System Development");
+    }
+}
